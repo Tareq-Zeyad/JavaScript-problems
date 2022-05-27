9f1e64c8ed92fde692a852051ae9cf1d4eea1ce4
Tariq Zeyad Sharaia <tariq.z.sharaia@gmail.com>
2022-05-28 00:32:25 +0300

    Ceaser cipher solved

--- intermediate_problems.js
+++ intermediate_problems.js
@@ -119,9 +119,40 @@ function convertToRoman(num) {
   }
   console.log(romanStr);
   return romanStr;
-}
+};
 
 convertToRoman(29);
 
 
+// Problem 6
+/*
+One of the simplest and most widely known ciphers is a Caesar cipher, also known as a shift cipher. In a shift cipher the meanings of the letters are shifted by some set amount.
 
+A common modern use is the ROT13 cipher, where the values of the letters are shifted by 13 places. Thus A ↔ N, B ↔ O and so on.
+
+Write a function which takes a ROT13 encoded string as input and returns a decoded string.
+
+All letters will be uppercase. Do not transform any non-alphabetic character (i.e. spaces, punctuation), but do pass them on.
+*/
+function rot13(str) {
+  let charCode = 0;
+  let strArr = [];
+  strArr = str.split("");
+  for (let i = 0; i < strArr.length; i++) {
+    charCode = strArr[i].charCodeAt();
+    if (charCode >= 65 && charCode <= 90) {
+      if (charCode > 77) {
+        charCode = charCode - 13;
+      }
+      else {
+        charCode = charCode + 13;
+      }
+
+      strArr.splice(i,1,String.fromCharCode(charCode));
+    }
+  }
+  console.log(strArr);
+  return strArr.join('');
+}
+
+rot13("SERR PBQR PNZC");
